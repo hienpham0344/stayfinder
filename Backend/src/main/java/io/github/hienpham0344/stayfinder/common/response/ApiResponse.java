11567335df399cbbb1f3b2f5dd575dba13e9a9e6
hienpham0344 <24520477@gm.uit.edu.vn>
2026-08-08 16:39:55 +0700

    feat: API response

---
+++ Backend/src/main/java/io/github/hienpham0344/stayfinder/common/response/ApiResponse.java
@@ -0,0 +1,17 @@
+package io.github.hienpham0344.stayfinder.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    int code;
+    String message;
+    T result;
+}
